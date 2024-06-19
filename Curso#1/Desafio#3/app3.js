39986e412a4fbaa2cf8de2a9526a1cdc1f86311d
Spektrum77 <85252093+Spektrum77@users.noreply.github.com>
2024-06-19 15:58:11 -0500

    Desafio#3

---
+++ Curso#1/Desafio#3/app3.js
@@ -0,0 +1,29 @@
+// Desafio #3
+// 1. Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+let contador = 1
+while (contador <= 10) {
+    alert (contador)    
+    console.log(contador)
+    contador++
+}
+
+// 2. Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+let contador2 = 10
+while (contador2 >= 0) {
+    alert (contador2)    
+    console.log(contador2)
+    contador2--
+}
+
+// 3. Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+let numero = prompt("Ingresa un numero")
+let contador3 = 0
+while (contador3 <= numero) {
+    alert (contador3)    
+    console.log(contador3)
+    contador3++
+}
+
+// 4. Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+
+--/* ejercicio repetido */--
